12ed1c7bea008d1a0670f37b32395d0676ce40bf
JulioKobayashiEbury <julio.kobayashi@ebury.com>
2025-05-09 11:33:05 -0300

    [BankingAPI] working on resolving module and package issues

--- code/cmd/main.go
+++ code/cmd/main.go
@@ -1,6 +1,6 @@
 package main
 
-import "BankingAPI.com/ports"
+import "github.com/JulioKobayashiEbury/BankingAPI/ports"
 
 func main() {
 	BankingAPI_test.server()
--- code/pkg/ports/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/JulioKobayashiEbury/BankingAPI/ports
-
-go 1.24.1
