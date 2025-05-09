9f513b87b229ee4d1ba9977cc0745689cc44d088
JulioKobayashiEbury <julio.kobayashi@ebury.com>
2025-05-09 12:57:17 -0300

    [BankingAPI] Working on userResponse

--- code/pkg/ports/ports.go
+++ code/pkg/ports/ports.go
@@ -1,9 +1,26 @@
 package ports
 
 import (
-	"fmt"
+	"encoding/json"
+	"net/http"
 )
 
+type User struct {
+	Name  string `json:"name" xml:"name"`
+	Email string `json:"email" xml:"email"`
+}
+
 func Server() {
-	fmt.Println("Server started")
+	http.HandleFunc("/user", userResponse)
+	http.ListenAndServe(":8080", nil)
+}
+
+func userResponse(w http.ResponseWriter, r *http.Request) {
+	user := User{
+		Name:  "Julio",
+		Email: "julio@gmail.com",
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(user)
 }
